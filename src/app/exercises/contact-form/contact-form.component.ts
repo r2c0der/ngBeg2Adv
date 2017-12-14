b3fb906c53d87a060a12d3fc03904748d058b7cb
r2koder <r2koder@gmail.com>
2017-12-14 12:05:17 -0500

    Building out lesson contact form

--- src/app/exercises/contact-form/contact-form.component.ts
+++ src/app/exercises/contact-form/contact-form.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactFormComponent implements OnInit {
 
+  contactMethods = [
+    {
+      id: 1,
+      name: 'Email'
+    },
+    {
+      id: 2,
+      name: 'Mobile'
+    },
+    {
+      id: 3,
+      name: 'Office Phone'
+    },
+    {
+      id: 4,
+      name: 'Fax'
+    },
+    {
+      id: 5,
+      name: 'Office Mail'
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
+  }
+
+  formSubmit(formObj: any){
+    console.log(formObj);
   }
 
 }

16dba428e27487522ec56be92b92f773a223530f
r2koder <r2koder@gmail.com>
2017-12-18 22:50:59 -0500

    Form Builder, Form Groups and Form Arrays

--- src/app/exercises/forms/newcourseform/newcourseform.component.ts
+++ src/app/exercises/forms/newcourseform/newcourseform.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormArray, FormControl, FormGroup} from "@angular/forms";
+import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 
 
@@ -9,13 +9,47 @@ import { FormArray, FormControl, FormGroup} from "@angular/forms";
   styleUrls: ['./newcourseform.component.css']
 })
 export class NewCourseFormComponent  {
+  form: any;
 
-  form = new FormGroup({
+/*  form = new FormGroup({
+    name: new FormControl(" ", Validators.required),
+    contact: new FormGroup({
+      email: new FormControl(),
+      phone: new FormControl(),
+    }),
     topics: new FormArray([])
-  });
+  });*/
+
+  constructor(_formBuilder: FormBuilder){
+  this.form =   _formBuilder.group({
+      name: ['', Validators.required],
+      contact: _formBuilder.group({
+        email: [],
+        phone: []
+      }),
+      topics: _formBuilder.array([])
+    })
+
+  }
+
+
+
+
+
 
   addTopic(topic: HTMLInputElement){
-    (this.form.get('topics') as FormArray).push(new FormControl(topic.value));
+    this.topics.push(new FormControl(topic.value));
+    topic.value = '';
+  }
+
+  removeTopic(topic: FormControl){
+  let index =   this.topics.controls.indexOf(topic);
+  this.topics.removeAt(index);
+
+  }
+  get topics(){
+    return this.form.get('topics') as FormArray;
+
   }
 
 
